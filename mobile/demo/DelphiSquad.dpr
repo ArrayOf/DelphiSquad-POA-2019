b9f37fde57ba5bdeff7bf82246cd9dca8a783feb
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2019-09-22 22:01:57 -0300

    Projeto mobile da Apresentacao

---
+++ mobile/demo/DelphiSquad.dpr
@@ -0,0 +1,22 @@
+program DelphiSquad;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  GoogleMaps in 'GoogleMaps.pas' {FormGoogleMaps},
+  RestTimer in 'RestTimer.pas' {FormRestTimer},
+  LocationSensor in 'LocationSensor.pas' {FormLocationSensor},
+  Rest in 'Rest.pas' {FormRest};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.FormFactor.Orientations := [TFormOrientation.Portrait];
+  //Application.CreateForm(TFormGoogleMaps, FormGoogleMaps);
+  Application.CreateForm(TFormRestTimer, FormRestTimer);
+  Application.CreateForm(TFormRest, FormRest);
+  //Application.CreateForm(TFormRest, FormRest);
+  //Application.CreateForm(TFormLocationSensor, FormLocationSensor);
+  Application.Run;
+end.
