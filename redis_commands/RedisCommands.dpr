eb147425eff6a027b12c624bff89cfd3557717a9
Mario Guedes <mario.guedes@arrayof.com.br>
2019-09-24 15:01:26 -0300

    Versão viabilizada do redis_commands

--- redis_commands/RedisCommands.dpr
+++ redis_commands/RedisCommands.dpr
@@ -2,7 +2,8 @@ program RedisCommands;
 
 uses
   Vcl.Forms,
-  Main in 'Main.pas' {Form1};
+  Main in 'Main.pas' {Form1},
+  Rules in 'Rules.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
